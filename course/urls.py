2a510d42c82783190eba1110fc22584ffc72309a
AspiranDrago <sanekclubstr@mail.ru>
2018-02-26 11:53:02 +0500

    Добавлен API по внесению/очистке значений курсов

---
+++ course/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.course_list, name='course_list'),
+	url(r'^api/add/(?P<name>\w+)/val=(?P<val>[\d,\.]+)&prop=(?P<prop>\d+)$', views.course_add, name='course_add'),
+	url(r'^api/clear/(?P<name>\w+)$', views.course_clear, name='course_clear'),
+]
--- course/views.py
+++ course/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Course, Currency
 
 # Create your views here.
+def course_list(request):
+    return render(request, 'course/course_list.html', {})
+	
+
+def course_add(request, name, val, prop):
+	name = name.replace('_', ' ')
+	if(Currency.objects.filter(name=name)):
+		new_course = Course(currency=name, value=float(val), prop=int(prop))
+		new_course.save()
+		return render(request, 'course/course_add.html', {'status': 0})
+	else:
+		return render(request, 'course/course_add.html', {'status': 1})
+
+		
+def course_clear(request, name):
+	name = name.replace('_', ' ')
+	if(Currency.objects.filter(name=name)):
+		Course.objects.filter(currency=name).delete()
+		return render(request, 'course/course_clear.html', {'status': 0})
+	else:
+		return render(request, 'course/course_clear.html', {'status': 1})
